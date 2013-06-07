b3bcdf311add09d8651b8c2f198cf9c4031e2117
Daniel Wood <daniel.j.wood11@gmail.com>
2013-06-07 13:06:19 -0400

    updated data

--- site.js
+++ site.js
@@ -3,318 +3,317 @@
 $(document).ready(function() {
 //Load state price json
     var data = [
-    
     {
         "state": "US Average",
         "stateabbrev": "US",
-        "electric_raw": "11.47",
-        "eproxy": "1.174528"
+        "electric_raw": "11.59",
+        "eproxy": "1.14"
     },
-    {
+        {
         "state": "Alabama",
         "stateabbrev": "AL",
-        "electric_raw": "10.84",
-        "eproxy": "1.110016"
+        "electric_raw": "10.97",
+        "eproxy": "1.08"
     },
     {
         "state": "Alaska",
         "stateabbrev": "AK",
-        "electric_raw": "16.68",
-        "eproxy": "1.708032"
+        "electric_raw": "17.72",
+        "eproxy": "1.75"
     },
     {
         "state": "Arizona",
         "stateabbrev": "AZ",
-        "electric_raw": "10.25",
-        "eproxy": "1.0496"
+        "electric_raw": "10.87",
+        "eproxy": "1.07"
     },
     {
         "state": "Arkansas",
         "stateabbrev": "AR",
-        "electric_raw": "8.74",
-        "eproxy": "0.894976"
+        "electric_raw": "9.07",
+        "eproxy": "0.90"
     },
     {
         "state": "California",
         "stateabbrev": "CA",
-        "electric_raw": "16.40",
-        "eproxy": "1.67936"
+        "electric_raw": "15.34",
+        "eproxy": "1.51"
     },
     {
         "state": "Colorado",
         "stateabbrev": "CO",
-        "electric_raw": "11.06",
-        "eproxy": "1.132544"
+        "electric_raw": "11.37",
+        "eproxy": "1.12"
     },
     {
         "state": "Connecticut",
         "stateabbrev": "CT",
-        "electric_raw": "17.05",
-        "eproxy": "1.74592"
+        "electric_raw": "17.22",
+        "eproxy": "1.70"
     },
     {
         "state": "Delaware",
         "stateabbrev": "DE",
-        "electric_raw": "12.85",
-        "eproxy": "1.31584"
+        "electric_raw": "13.02",
+        "eproxy": "1.29"
     },
     {
         "state": "District of Columbia",
         "stateabbrev": "DC",
-        "electric_raw": "12.06",
-        "eproxy": "1.234944"
+        "electric_raw": "12.14",
+        "eproxy": "1.20"
     },
     {
         "state": "Florida",
         "stateabbrev": "FL",
-        "electric_raw": "11.34",
-        "eproxy": "1.161216"
+        "electric_raw": "11.19",
+        "eproxy": "1.10"
     },
     {
         "state": "Georgia",
         "stateabbrev": "GA",
-        "electric_raw": "10.25",
-        "eproxy": "1.0496"
+        "electric_raw": "10.64",
+        "eproxy": "1.05"
     },
     {
         "state": "Hawaii",
         "stateabbrev": "HI",
-        "electric_raw": "37.86",
-        "eproxy": "3.876864"
+        "electric_raw": "37.43",
+        "eproxy": "3.69"
     },
     {
         "state": "Idaho",
         "stateabbrev": "ID",
-        "electric_raw": "8.59",
-        "eproxy": "0.879616"
+        "electric_raw": "8.46",
+        "eproxy": "0.84"
     },
     {
         "state": "Illinois",
         "stateabbrev": "IL",
-        "electric_raw": "10.14",
-        "eproxy": "1.038336"
+        "electric_raw": "10.08",
+        "eproxy": "0.99"
     },
     {
         "state": "Indiana",
         "stateabbrev": "IN",
-        "electric_raw": "9.98",
-        "eproxy": "1.021952"
+        "electric_raw": "10.49",
+        "eproxy": "1.04"
     },
     {
         "state": "Iowa",
         "stateabbrev": "IA",
-        "electric_raw": "10.13",
-        "eproxy": "1.037312"
+        "electric_raw": "10.48",
+        "eproxy": "1.03"
     },
     {
         "state": "Kansas",
         "stateabbrev": "KS",
-        "electric_raw": "10.62",
-        "eproxy": "1.087488"
+        "electric_raw": "11.13",
+        "eproxy": "1.10"
     },
     {
         "state": "Kentucky",
         "stateabbrev": "KY",
-        "electric_raw": "9.25",
-        "eproxy": "0.9472"
+        "electric_raw": "9.33",
+        "eproxy": "0.92"
     },
     {
         "state": "Louisiana",
         "stateabbrev": "LA",
-        "electric_raw": "8.76",
-        "eproxy": "0.897024"
+        "electric_raw": "9.14",
+        "eproxy": "0.90"
     },
     {
         "state": "Maine",
         "stateabbrev": "ME",
-        "electric_raw": "14.61",
-        "eproxy": "1.496064"
+        "electric_raw": "14.16",
+        "eproxy": "1.40"
     },
     {
         "state": "Maryland",
         "stateabbrev": "MD",
-        "electric_raw": "12.50",
-        "eproxy": "1.28"
+        "electric_raw": "12.55",
+        "eproxy": "1.24"
     },
     {
         "state": "Massachusetts",
         "stateabbrev": "MA",
-        "electric_raw": "14.28",
-        "eproxy": "1.462272"
+        "electric_raw": "14.77",
+        "eproxy": "1.46"
     },
     {
         "state": "Michigan",
         "stateabbrev": "MI",
-        "electric_raw": "13.65",
-        "eproxy": "1.39776"
+        "electric_raw": "14.08",
+        "eproxy": "1.39"
     },
     {
         "state": "Minnesota",
         "stateabbrev": "MN",
-        "electric_raw": "11.05",
-        "eproxy": "1.13152"
+        "electric_raw": "11.32",
+        "eproxy": "1.12"
     },
     {
         "state": "Mississippi",
         "stateabbrev": "MS",
-        "electric_raw": "9.99",
-        "eproxy": "1.022976"
+        "electric_raw": "10.34",
+        "eproxy": "1.02"
     },
     {
         "state": "Missouri",
         "stateabbrev": "MO",
-        "electric_raw": "8.93",
-        "eproxy": "0.914432"
+        "electric_raw": "9.56",
+        "eproxy": "0.94"
     },
     {
         "state": "Montana",
         "stateabbrev": "MT",
-        "electric_raw": "9.81",
-        "eproxy": "1.004544"
+        "electric_raw": "10.05",
+        "eproxy": "0.99"
     },
     {
         "state": "Nebraska",
         "stateabbrev": "NE",
-        "electric_raw": "8.72",
-        "eproxy": "0.892928"
+        "electric_raw": "9.42",
+        "eproxy": "0.93"
     },
     {
         "state": "Nevada",
         "stateabbrev": "NV",
-        "electric_raw": "11.24",
-        "eproxy": "1.150976"
+        "electric_raw": "11.90",
+        "eproxy": "1.17"
     },
     {
         "state": "New Hampshire",
         "stateabbrev": "NH",
-        "electric_raw": "16.06",
-        "eproxy": "1.644544"
+        "electric_raw": "16.55",
+        "eproxy": "1.63"
     },
     {
         "state": "New Jersey",
         "stateabbrev": "NJ",
-        "electric_raw": "15.30",
-        "eproxy": "1.56672"
+        "electric_raw": "15.33",
+        "eproxy": "1.51"
     },
     {
         "state": "New Mexico",
         "stateabbrev": "NM",
-        "electric_raw": "10.88",
-        "eproxy": "1.114112"
+        "electric_raw": "11.31",
+        "eproxy": "1.12"
     },
     {
         "state": "New York",
         "stateabbrev": "NY",
-        "electric_raw": "18.02",
-        "eproxy": "1.845248"
+        "electric_raw": "18.26",
+        "eproxy": "1.80"
     },
     {
         "state": "North Carolina",
         "stateabbrev": "NC",
-        "electric_raw": "10.19",
-        "eproxy": "1.043456"
+        "electric_raw": "10.29",
+        "eproxy": "1.02"
     },
     {
         "state": "North Dakota",
         "stateabbrev": "ND",
-        "electric_raw": "7.71",
-        "eproxy": "0.789504"
+        "electric_raw": "8.39",
+        "eproxy": "0.83"
     },
     {
         "state": "Ohio",
         "stateabbrev": "OH",
-        "electric_raw": "11.01",
-        "eproxy": "1.127424"
+        "electric_raw": "11.30",
+        "eproxy": "1.12"
     },
     {
         "state": "Oklahoma",
         "stateabbrev": "OK",
-        "electric_raw": "8.01",
-        "eproxy": "0.820224"
+        "electric_raw": "9.36",
+        "eproxy": "0.92"
     },
     {
         "state": "Oregon",
         "stateabbrev": "OR",
-        "electric_raw": "9.65",
-        "eproxy": "0.98816"
+        "electric_raw": "9.68",
+        "eproxy": "0.96"
     },
     {
         "state": "Pennsylvania",
         "stateabbrev": "PA",
-        "electric_raw": "12.35",
-        "eproxy": "1.26464"
+        "electric_raw": "12.30",
+        "eproxy": "1.21"
     },
     {
         "state": "Rhode Island",
         "stateabbrev": "RI",
-        "electric_raw": "14.80",
-        "eproxy": "1.51552"
+        "electric_raw": "16.23",
+        "eproxy": "1.60"
     },
     {
         "state": "South Carolina",
         "stateabbrev": "SC",
-        "electric_raw": "11.63",
-        "eproxy": "1.190912"
+        "electric_raw": "10.96",
+        "eproxy": "1.08"
     },
     {
         "state": "South Dakota",
         "stateabbrev": "SD",
-        "electric_raw": "9.07",
-        "eproxy": "0.928768"
+        "electric_raw": "9.60",
+        "eproxy": "0.95"
     },
     {
         "state": "Tennessee",
         "stateabbrev": "TN",
-        "electric_raw": "10.03",
-        "eproxy": "1.027072"
+        "electric_raw": "9.79",
+        "eproxy": "0.97"
     },
     {
         "state": "Texas",
         "stateabbrev": "TX",
-        "electric_raw": "10.89",
-        "eproxy": "1.115136"
+        "electric_raw": "11.03",
+        "eproxy": "1.09"
     },
     {
         "state": "Utah",
         "stateabbrev": "UT",
-        "electric_raw": "9.69",
-        "eproxy": "0.992256"
+        "electric_raw": "9.79",
+        "eproxy": "0.97"
     },
     {
         "state": "Vermont",
         "stateabbrev": "VT",
-        "electric_raw": "17.73",
-        "eproxy": "1.815552"
+        "electric_raw": "14.64",
+        "eproxy": "1.44"
     },
     {
         "state": "Virginia",
         "stateabbrev": "VA",
-        "electric_raw": "9.99",
-        "eproxy": "1.022976"
+        "electric_raw": "10.49",
+        "eproxy": "1.04"
     },
     {
         "state": "Washington",
         "stateabbrev": "WA",
-        "electric_raw": "8.50",
-        "eproxy": "0.8704"
+        "electric_raw": "8.48",
+        "eproxy": "0.84"
     },
     {
         "state": "West Virginia",
         "stateabbrev": "WV",
-        "electric_raw": "9.37",
-        "eproxy": "0.959488"
+        "electric_raw": "9.47",
+        "eproxy": "0.93"
     },
     {
         "state": "Wisconsin",
         "stateabbrev": "WI",
-        "electric_raw": "13.10",
-        "eproxy": "1.34144"
+        "electric_raw": "13.40",
+        "eproxy": "1.32"
     },
     {
         "state": "Wyoming",
         "stateabbrev": "WY",
-        "electric_raw": "9.51",
-        "eproxy": "0.973824"
+        "electric_raw": "9.83",
+        "eproxy": "0.97"
     }
 ]
         //Populate select dropdown menu
@@ -330,97 +329,97 @@ $(document).ready(function() {
     {
         "loc": "US",
         "loc_abbrev": "US",
-        "price": "3.645"
+        "price": "3.65"
     },
     {
         "loc": "East Coast",
         "loc_abbrev": "EC",
-        "price": "3.501"
+        "price": "3.48"
     },
     {
         "loc": "New England",
         "loc_abbrev": "NE",
-        "price": "3.591"
+        "price": "3.58"
     },
     {
         "loc": "Central Atlantic",
         "loc_abbrev": "CAT",
-        "price": "3.557"
+        "price": "3.54"
     },
     {
         "loc": "Lower Atlantic",
         "loc_abbrev": "LAT",
-        "price": "3.431"
+        "price": "3.41"
     },
     {
         "loc": "Midwest",
         "loc_abbrev": "MW",
-        "price": "3.779"
+        "price": "3.84"
     },
     {
         "loc": "Gulf Coast",
         "loc_abbrev": "GC",
-        "price": "3.366"
+        "price": "3.34"
     },
     {
         "loc": "Rocky Mountain",
         "loc_abbrev": "RM",
-        "price": "3.725"
+        "price": "3.73"
     },
     {
         "loc": "West Coast",
         "loc_abbrev": "WC",
-        "price": "3.933"
+        "price": "3.89"
     },
     {
         "loc": "West Coast less California",
         "loc_abbrev": "WCLC",
-        "price": "3.779"
+        "price": "3.74"
     },
     {
         "loc": "California",
         "loc_abbrev": "CA",
-        "price": "4.022"
+        "price": "3.98"
     },
     {
         "loc": "Colorado",
         "loc_abbrev": "CO",
-        "price": "3.82"
+        "price": "3.78"
     },
     {
         "loc": "Florida",
         "loc_abbrev": "FL",
-        "price": "3.442"
+        "price": "3.42"
     },
     {
         "loc": "Massachusetts",
         "loc_abbrev": "MA",
-        "price": "3.511"
+        "price": "3.50"
     },
     {
         "loc": "Minnesota",
         "loc_abbrev": "MN",
-        "price": "4.022"
+        "price": "3.84"
     },
     {
         "loc": "New York",
         "loc_abbrev": "NY",
-        "price": "3.72"
+        "price": "3.70"
     },
     {
         "loc": "Ohio",
         "loc_abbrev": "OH",
-        "price": "3.676"
+        "price": "3.86"
     },
     {
         "loc": "Texas",
         "loc_abbrev": "TX",
-        "price": "3.397"
+        "price": "3.37"
     },
     {
         "loc": "Washington",
         "loc_abbrev": "WA",
-        "price": "3.962"
+        "price": "3.87"
     }
 ]
 
